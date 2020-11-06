f9f96733c09816f8adbe5dfb8ce433a8a294cbd5
tallgy <1776232657@qq.com>
2020-11-06 10:42:27 +0800

    创建了一个前端项目目录

---
+++ scientfic_research_management/src/components/museuitest.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>muse ui</h1>
+    <mu-text-field v-model="value6" label="Label Float" prefix="$" label-float ></mu-text-field><br/>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
