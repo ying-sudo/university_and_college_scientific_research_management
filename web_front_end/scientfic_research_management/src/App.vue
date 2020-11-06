74b73243aaba8b45813f61131f216f163ce89cd3
ying-sudo <3214364139@qq.com>
2020-11-06 11:19:29 +0800

    new web_front_end

---
+++ web_front_end/scientfic_research_management/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <ss></ss>
+    <muse></muse>
+    <p>ssssss</p>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import ss from './components/ss.vue'
+import muse from './components/museuitest.vue'
+
+export default {
+  name: 'app',
+  components: {
+    // HelloWorld,
+    ss,
+    muse
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
