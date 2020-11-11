7c6829504b74f78c9cdb3f20bd78ef35b22baf39
ying-sudo <3214364139@qq.com>
2020-11-11 19:33:46 +0800

    增加了commons的table组件以及static的table行列数据

---
+++ web_front_end/scientfic_research_management/src/components/table/check_table/AchievementCheckTable.vue
@@ -0,0 +1,49 @@
+<template>
+<!-- 成果考核列表 -->
+  <div>
+    <!-- 调用抽象成CommonsTable标签的公共表格组件 -->
+    <CommonsTable
+      v-if="tableData != null"
+      :itemOptions="itemOptions"
+      :tableData="tableData"
+    >
+    </CommonsTable>
+  </div>
+</template>
+
+<script>
+import CommonsTable from "@/components/table/Commons/CommonsTable";
+export default {
+
+  data() {
+    return {
+      //返回到页面中的表格
+      itemOptions: [],
+
+      //返回到表格中的数据
+      tableData: [],
+    };
+  },
+  components: {
+    CommonsTable,
+  },
+  mounted: function () {
+    //从相应json文件读取表格的列
+    this.axios
+      .get("http://localhost:8080/static/table/item_option/check_table/AchievementCheckTable.json")
+      .then((res) => {
+        this.itemOptions = res.data;
+      });
+
+    //从相应json文件中读取表格数据
+    this.axios
+      .get("http://localhost:8080/static/table/table_data/project_table/ProjectTableData.json")
+      .then((res) => {
+        this.tableData = res.data;
+      });
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ web_front_end/scientfic_research_management/src/components/table/test.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="block">
+    <!--@page-sizes:选择一页有多少条数据
+        @page-size:默认一页10条数据
+        @total:总共的数据行数
+        @layout:在页面显示的效果 -->
+    <el-pagination
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :current-page="currentPage4"
+      :page-sizes="[10, 20, 50, 100]"
+      :page-size="10"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="400"
+    >
+    </el-pagination>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    handleSizeChange(val) {
+      console.log(`每页 ${val} 条`);
+    },
+    handleCurrentChange(val) {
+      console.log(`当前页: ${val}`);
+    },
+  },
+  data() {
+    //开始指向第一页
+    return {
+      currentPage1: 1,
+      currentPage2: 1,
+      currentPage3: 1,
+      currentPage4: 1,
+    };
+  },
+};
+</script>
+
+<style>
+</style>
