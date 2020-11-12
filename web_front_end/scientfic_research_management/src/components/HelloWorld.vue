a3253d7445de4d2b07f42c6e91b7cd191f320b29
tallgy <1776232657@qq.com>
2020-11-12 15:15:11 +0800

    基本表单的完成

--- web_front_end/scientfic_research_management/src/components/HelloWorld.vue
+++ web_front_end/scientfic_research_management/src/components/HelloWorld.vue
@@ -1,15 +1,16 @@
-﻿<template>
+<template>
   <div>
 
     <MyButton></MyButton>
-
+	
+	</div>
 </template>
 
 
 
 <script>
 
-  import MyButton from './commons/forms/button.vue'
+  import MyButton from './commons/forms/Button.vue'
 
   export default {
     components: {
