8fccf91cc525b15aeb3c57cae1610b70b6b89f49
tallgy <1776232657@qq.com>
2020-11-16 09:56:32 +0800

    登录页面

--- web_front_end/scientfic_research_management/src/router/index.js
+++ web_front_end/scientfic_research_management/src/router/index.js
@@ -1,28 +1,29 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-// import HelloWorld from '@/components/HelloWorld'
+import HelloWorld from '@/components/HelloWorld'
 import BaseHeaderNav from '@/components/views/BaseHeaderNav'
 import BaseSideNav from '@/components/views/BaseSideNav'
 import Home from '@/components/views/Home'
-import First from '@/components/views/First'
+import Login from '@/components/login/login.vue'
+import InitPassword from '@/components/login/InitPassword.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    {
+      path: '/helloworld',
+      name: 'helloworld',
+      component: HelloWorld
+    },
     // {
     //   path: '/',
-    //   name: 'HelloWorld',
-    //   component: HelloWorld
+    //   redirect: "/login"
     // },
     {
-      path: '/',
-      name: 'Home',
-      component: Home
-    },
-    {
       path: '/home',
-      redirect: "/"
+      name: 'Home',
+      components: Home
     },
     {
       path: '/header',
@@ -34,9 +35,15 @@ export default new Router({
       name: 'BaseSideNav',
       component: BaseSideNav
     },
-    // {
-    //   path: '/first',
-    //   component: First
-    // }
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/InitPWD',
+      name: 'InitPWD',
+      components: InitPassword
+    }
   ]
 })
