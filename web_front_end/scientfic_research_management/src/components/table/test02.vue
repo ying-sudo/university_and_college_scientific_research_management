99c2af36d660f118fc26ee8e4ce2fa2587f80e35
ying-sudo <3214364139@qq.com>
2020-11-21 11:43:11 +0800

    删除了原有的commonstable文件夹

---
+++ web_front_end/scientfic_research_management/src/components/table/test02.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="user_authority_operation_icon">
+    <i class="el-icon-edit"></i>
+    <i class="el-icon-share"></i>
+    <i class="el-icon-delete"></i>
+    <a-icon type="eye" />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.user_authority_operation_icon{
+    color:#545C64;
+    font-size: 30px;
+    
+}
+</style>
--- web_front_end/scientfic_research_management/src/router/index.js
+++ web_front_end/scientfic_research_management/src/router/index.js
@@ -1,11 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-
+import BaseHeaderNav from '@/components/views/BaseHeaderNav'
+import BaseSideNav from '@/components/views/BaseSideNav'
+import Home from '@/components/views/Home'
 import Login from '@/components/login/Login.vue'
 import InitPassword from '@/components/login/InitPassword.vue'
 // import Table from '@/components/table/table_interface/CommonsTableImpl'
 import Table from '@/components/table/table_impl/SPTableEnity'
+import Icon from '@/components/table/test02'
 
 Vue.use(Router)
 
@@ -23,7 +26,7 @@ export default new Router({
     {
       path: '/home',
       name: 'Home',
-      component: resolve => require(['@/components/views/Home'], resolve)
+      component: Home
     },
     // {
     //   path: '/header',
@@ -44,6 +47,11 @@ export default new Router({
       path: '/initPWD',
       name: 'InitPassword',
       component: InitPassword
+    },
+    {
+      path: '/@',
+      name: 'Icon',
+      component: Icon
     },
     // 表格访问地址根据tableKey参数决定表格(如表头以及表格中的数据)
     {
